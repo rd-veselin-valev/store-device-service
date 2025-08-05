5b16474301ad01e2e87ee7a060429ca31635e651
Veselin-Valev-Rewe <veselin.valev@rewe-group.com>
2025-08-05 09:44:51 +0300

    Initial commit

---
+++ src/main/java/com/example/store_device_service/StoreDeviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_device_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDeviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDeviceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/store_device_service/StoreDeviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_device_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDeviceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
